2abc6c7c83257e6031c79dcb1a55940d52c0752f
surafel_fikru <surafelfikru76@gmail.com>
2025-05-22 13:12:10 +0400

    implement clear

--- src/mork_client.py
+++ src/mork_client.py
@@ -2,7 +2,7 @@ import httpx
 from httpx import URL
 from typing import Optional
 import os
-
+from urllib.parse import quote
 
 class MorkClient:
     def __init__(self, url: Optional[str| URL] = os.environ.get("MORK_URL")):
@@ -31,9 +31,58 @@ class MorkClient:
         async with httpx.AsyncClient() as client:
             return await client.request(method, url, **kwargs)
 
-    async def stop(self):
-        await self._request(method="GET", path="/stop/", params={"wait_for_idle": ""})
+    async def _mork_to_path(self, mork_path: str):
+        """changes mork code to valid path parameters"""
 
+        lines = mork_path.split('\n')
+        lines = [line.strip() for line in lines]
+        lines = [line for line in lines if line != ""]  # remove empty lines
+        path = "/".join(lines)
+
+        return path
 
     async def busywait(self, timeout: int = 2000):
         await self._request(method="GET", path=f"/busywait/{timeout}")
+
+    async def clear(self, expr: str):
+        path = quote(expr.strip())
+
+        return await self._request(
+            method="GET",
+            path=f"/clear/{path}"
+        )
+
+    async def count(self):
+        pass
+
+    async def export(self):
+        pass
+
+    async def import_(self):
+        pass
+
+    async def status(self):
+        pass
+
+    async def stop(self):
+        return await self._request(
+            method="GET",
+            path="/stop/",
+            params={"wait_for_idle": ""}
+        )
+
+    async def upload(self):
+        pass
+
+    async def transform(self):
+        pass
+
+    async def metta_thread(self):
+        pass
+
+    async def metta_thread_suspend(self):
+        pass
+
+    async def transform_multi_multi(self):
+        pass
+
